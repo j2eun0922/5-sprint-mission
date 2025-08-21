f763a4ae701b89c9fdb6adcc220ec0bc418f05dc
j2eun0922 <j2eun0922@naver.com>
2025-08-21 11:23:47 +0900

    feat: implement binaryContent find and find all REST API

--- src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/BinaryContentController.java
@@ -4,12 +4,11 @@ import com.sprint.mission.discodeit.dto.api.ApiResult;
 import com.sprint.mission.discodeit.entity.BinaryContent;
 import com.sprint.mission.discodeit.service.BinaryContentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,13 +18,16 @@ public class BinaryContentController {
     private final BinaryContentService binaryContentService;
 
     @RequestMapping(value = "/find/{id}", method = RequestMethod.GET)
-    public ResponseEntity<byte[]> findById(@PathVariable("id") UUID id) {
+    public ResponseEntity<ApiResult<BinaryContent>> findById(@PathVariable("id") UUID id) {
         BinaryContent binaryContent = binaryContentService.find(id);
 
-        return ResponseEntity.ok()
-                .contentType(MediaType.parseMediaType(binaryContent.getContentType()))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=\"" + binaryContent.getFileName() + "\"")
-                .contentLength(binaryContent.getSize())
-                .body(binaryContent.getBytes());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(binaryContent));
+    }
+
+    @RequestMapping(value = "/findAllByIdIn", method = RequestMethod.GET)
+    public ResponseEntity<ApiResult<List<BinaryContent>>> findAllByIdIn(@RequestParam("id") List<UUID> id) {
+        List<BinaryContent> binaryContent = binaryContentService.findAllByIdIn(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResult.ok(binaryContent));
     }
 }
