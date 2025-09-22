0ff1701ae05c1287eb3bfa77604bce7577c019de
j2eun0922 <j2eun0922@naver.com>
2025-09-22 13:20:17 +0900

    chore : 베이스코드

--- schema.sql
+++ schema.sql
@@ -1,88 +1,135 @@
-DROP TABLE IF EXISTS channels CASCADE;
-DROP TABLE IF EXISTS binary_contents CASCADE;
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS binary_contents CASCADE;
 DROP TABLE IF EXISTS user_statuses CASCADE;
-DROP TABLE IF EXISTS read_statuses CASCADE;
+DROP TABLE IF EXISTS channels CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
 DROP TABLE IF EXISTS message_attachments CASCADE;
+DROP TABLE IF EXISTS read_statuses CASCADE;
 
--- binary_contents 테이블
-CREATE TABLE binary_contents
-(
-    id           uuid PRIMARY KEY,
-    created_at   TIMESTAMP WITH TIME ZONE NOT NULL,
-    file_name    VARCHAR(255)             NOT NULL,
-    size         BIGINT                   NOT NULL,
-    content_type VARCHAR(100)             NOT NULL
-);
 
--- users 테이블
+-- 테이블
+-- User
 CREATE TABLE users
 (
     id         uuid PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE,
-    username   VARCHAR(50) UNIQUE       NOT NULL,
-    email      VARCHAR(100) UNIQUE      NOT NULL,
-    password   VARCHAR(60)              NOT NULL,
-    profile_id uuid,
-    CONSTRAINT fk_users_binary_contents FOREIGN KEY (profile_id) REFERENCES binary_contents (id) ON DELETE SET NULL
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    username   varchar(50) UNIQUE       NOT NULL,
+    email      varchar(100) UNIQUE      NOT NULL,
+    password   varchar(60)              NOT NULL,
+    profile_id uuid
 );
 
--- channels 테이블
-CREATE TABLE channels
+-- BinaryContent
+CREATE TABLE binary_contents
 (
-    id          uuid PRIMARY KEY,
-    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at  TIMESTAMP WITH TIME ZONE,
-    name        VARCHAR(100),
-    description VARCHAR(500),
-    type        VARCHAR(10)              NOT NULL CHECK ( type IN ('PUBLIC', 'PRIVATE'))
+    id           uuid PRIMARY KEY,
+    created_at   timestamp with time zone NOT NULL,
+    file_name    varchar(255)             NOT NULL,
+    size         bigint                   NOT NULL,
+    content_type varchar(100)             NOT NULL
+--     ,bytes        bytea        NOT NULL
 );
 
--- user_statuses 테이블
+-- UserStatus
 CREATE TABLE user_statuses
 (
     id             uuid PRIMARY KEY,
-    created_at     TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at     TIMESTAMP WITH TIME ZONE,
+    created_at     timestamp with time zone NOT NULL,
+    updated_at     timestamp with time zone,
     user_id        uuid UNIQUE              NOT NULL,
-    last_active_at TIMESTAMP WITH TIME ZONE,
-    CONSTRAINT fk_user_statuses_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    last_active_at timestamp with time zone NOT NULL
 );
 
--- messages 테이블
+-- Channel
+CREATE TABLE channels
+(
+    id          uuid PRIMARY KEY,
+    created_at  timestamp with time zone NOT NULL,
+    updated_at  timestamp with time zone,
+    name        varchar(100),
+    description varchar(500),
+    type        varchar(10)              NOT NULL
+);
+
+-- Message
 CREATE TABLE messages
 (
     id         uuid PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE,
-    content    TEXT,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone,
+    content    text,
     channel_id uuid                     NOT NULL,
-    author_id  uuid,
-    CONSTRAINT fk_messages_channels FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
-    CONSTRAINT fk_messages_users FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE SET NULL
+    author_id  uuid
 );
 
-
--- read_statuses 테이블
-CREATE TABLE read_statuses
-(
-    id         uuid PRIMARY KEY,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    updated_at TIMESTAMP WITH TIME ZONE,
-    channel_id uuid                     NOT NULL,
-    user_id    uuid,
-    CONSTRAINT uk_user_channel UNIQUE (user_id, channel_id),
-    CONSTRAINT fk_read_statuses_channels FOREIGN KEY (channel_id) REFERENCES channels (id) ON DELETE CASCADE,
-    CONSTRAINT fk_read_statuses_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
-);
-
--- message_attachments 테이블
+-- Message.attachments
 CREATE TABLE message_attachments
 (
     message_id    uuid,
     attachment_id uuid,
-    CONSTRAINT fk_massage_attachments_messages FOREIGN KEY (message_id) REFERENCES messages (id) ON DELETE CASCADE,
-    CONSTRAINT fk_massage_attachments_binary_contents FOREIGN KEY (attachment_id) REFERENCES binary_contents (id) ON DELETE CASCADE
+    PRIMARY KEY (message_id, attachment_id)
 );
+
+-- ReadStatus
+CREATE TABLE read_statuses
+(
+    id           uuid PRIMARY KEY,
+    created_at   timestamp with time zone NOT NULL,
+    updated_at   timestamp with time zone,
+    user_id      uuid                     NOT NULL,
+    channel_id   uuid                     NOT NULL,
+    last_read_at timestamp with time zone NOT NULL,
+    UNIQUE (user_id, channel_id)
+);
+
+
+-- 제약 조건
+-- User (1) -> BinaryContent (1)
+ALTER TABLE users
+    ADD CONSTRAINT fk_user_binary_content
+        FOREIGN KEY (profile_id)
+            REFERENCES binary_contents (id)
+            ON DELETE SET NULL;
+
+-- UserStatus (1) -> User (1)
+ALTER TABLE user_statuses
+    ADD CONSTRAINT fk_user_status_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE;
+
+-- Message (N) -> Channel (1)
+ALTER TABLE messages
+    ADD CONSTRAINT fk_message_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE;
+
+-- Message (N) -> Author (1)
+ALTER TABLE messages
+    ADD CONSTRAINT fk_message_user
+        FOREIGN KEY (author_id)
+            REFERENCES users (id)
+            ON DELETE SET NULL;
+
+-- MessageAttachment (1) -> BinaryContent (1)
+ALTER TABLE message_attachments
+    ADD CONSTRAINT fk_message_attachment_binary_content
+        FOREIGN KEY (attachment_id)
+            REFERENCES binary_contents (id)
+            ON DELETE CASCADE;
+
+-- ReadStatus (N) -> User (1)
+ALTER TABLE read_statuses
+    ADD CONSTRAINT fk_read_status_user
+        FOREIGN KEY (user_id)
+            REFERENCES users (id)
+            ON DELETE CASCADE;
+
+-- ReadStatus (N) -> User (1)
+ALTER TABLE read_statuses
+    ADD CONSTRAINT fk_read_status_channel
+        FOREIGN KEY (channel_id)
+            REFERENCES channels (id)
+            ON DELETE CASCADE;
---
+++ src/main/generated/com/sprint/mission/discodeit/mapper/PageResponseMapperImpl.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-09-22T13:08:02+0900",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.15 (Eclipse Adoptium)"
+)
+@Component
+public class PageResponseMapperImpl implements PageResponseMapper {
+}
